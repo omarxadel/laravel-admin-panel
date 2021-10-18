dcd2124899a7115c166e529cdd68c59f426d2592
Omar Adel <omarxadel21@gmail.com>
2021-10-18 14:10:27 +0200

    feat: create user and company seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("users")->insert([
+            "firstname" => "",
+            "lastname" => "",
+            "email" => "admin@admin.com",
+            "password" => Hash::make("password"),
+            "company" => 1,
+            "role" => "admin",
+        ]);
+    }
+}
