5f967fb746cd67bdde8c8741b366a643a2182fcd
Omar Adel <omarxadel21@gmail.com>
2021-11-14 14:31:10 +0200

    refractor: change images to local storage instead of using API

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -46,8 +46,8 @@
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             <div class="flex items-center">
                                                 <div class="flex-shrink-0 h-10 w-10">
-                                                    <img class="h-10 w-10 rounded-full" src="{{ $company->logo }}"
-                                                        alt="Logo">
+                                                    <img class="h-10 w-10 rounded-full"
+                                                        src="{{ asset('storage/' . $company->logo) }}" alt="Logo">
                                                 </div>
                                                 <div class="ml-4">
                                                     <div class="text-sm font-medium text-gray-900">
