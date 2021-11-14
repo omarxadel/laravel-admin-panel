5aab746ee76af2a1b99101a3ff232a8998480509
Omar Adel <omarxadel21@gmail.com>
2021-11-14 14:52:30 +0200

    fix: user name not showing in navbar

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -91,13 +91,13 @@
                             <button
                                 class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                 <img class="h-8 w-8 rounded-full object-cover"
-                                    src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->firstname }}" />
                             </button>
                             @else
                             <span class="inline-flex rounded-md">
                                 <button type="button"
                                     class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
-                                    {{ Auth::user()->name }}
+                                    {{ Auth::user()->firstname }}
 
                                     <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 20 20" fill="currentColor">
